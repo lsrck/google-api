9af12f7b6b51138c2b101bd391b4e029ae806b8f
Louis Aurick <louisaurick@ntymail.com>
2018-02-28 17:16:01 +0100

    marker+zomm added

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,7 +1,17 @@
 function myMap() {
-var mapProp= {
+  var mapProp= {
     center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    zoom:5,};
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  //marker
+  var myCenter = new google.maps.LatLng(51.508742,-0.120850); 
+  var marker = new google.maps.Marker({position:myCenter});
+    marker.setMap(map);
+  //markerzoom
+  google.maps.event.addListener(marker,'click',function() {
+    map.setZoom(9);
+    map.setCenter(marker.getPosition());
+  }); 
 }
+
+
